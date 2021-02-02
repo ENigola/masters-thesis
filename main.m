c4e4be96fe2b36a9668a622549f71ef67146f40c
ergo.nigola@gmail.com <ergo.nigola@gamil.com>
2021-02-02 20:51:18 +0200

    Change and refactor decoding performance measurement logic.

--- main.m
+++ main.m
@@ -1,4 +1,32 @@
 clear;
 addpath('binPolyLib', 'decoders');
-measureDecodersPerformance();
 
+nMaxCodes = 1000;
+nMessagesPerCode = 200;
+nRequiredFailures = 25;
+insertedErrorCounts = 80:100;
+
+decoders = {
+    @decodeBitFlip;
+    %@decodeBackflip;
+    %@decodeBlackGray;
+}; 
+
+frameErrorRates = zeros(length(decoders), length(insertedErrorCounts));
+for i = 1:length(decoders)
+    frameErrorRates(i, :) = measureDecoderPerformance(decoders{i}, ...
+        nMaxCodes, nMessagesPerCode, nRequiredFailures, insertedErrorCounts);
+end
+
+% Plot results
+figure;
+title('Frame error rates');
+xlabel('t');
+ylabel('FER');
+set(gca, 'YScale', 'log');
+hold on;
+grid on;
+for i = 1:length(decoders)
+    semilogy(insertedErrorCounts, frameErrorRates(i, :), 'DisplayName', func2str(decoders{i}));
+end
+legend show;
---
+++ measureDecoderPerformance.m
@@ -0,0 +1,48 @@
+function frameErrorRates = measureDecoderPerformance(decoder, nMaxCodes, nMessagesPerCode, nRequiredFailures, insertedErrorCounts)
+% Tests the success rate of the decoder decoders
+
+r = 4801; % block width/height
+w = 45; % row and column weight of each block
+maxIter = 100; % per decoding attempt
+
+n = 2 * r;
+% rngSeed = 1;
+% rng(rngSeed);   
+
+frameErrors = zeros(1, length(insertedErrorCounts));
+trialCounts = zeros(1, length(insertedErrorCounts));
+remainingErrorCountsPos = 1:length(insertedErrorCounts);
+
+for i = 1:nMaxCodes
+    if isempty(remainingErrorCountsPos)
+        break; 
+    end
+    fprintf('Code %g\n', i);
+    [H, G] = generateRandomCode(r, w);
+    [R, C] = indexNonZeroPos(H);
+    for j = 1:nMessagesPerCode
+        msg = randi([0, 1], 1, r);
+        codeword = mod(msg * G, 2);
+        for errorCountPos = remainingErrorCountsPos
+            errorCount = insertedErrorCounts(errorCountPos);
+            errorPos = randperm(n, errorCount);
+            codewordWithErrors = codeword;
+            codewordWithErrors(errorPos) = mod(codewordWithErrors(errorPos) + 1, 2);
+            decoded = decoder(R, C, codewordWithErrors, maxIter);
+            trialCounts(errorCountPos) = trialCounts(errorCountPos) + 1;
+            if ~isequal(decoded, codeword)
+                frameErrors(errorCountPos) = frameErrors(errorCountPos) + 1;
+                if frameErrors(errorCountPos) >= nRequiredFailures
+                    remainingErrorCountsPos(remainingErrorCountsPos == errorCountPos) = [];
+                end
+            end
+        end
+        if isempty(remainingErrorCountsPos)
+            break;
+        end
+    end 
+end
+
+frameErrorRates = frameErrors ./ trialCounts;
+end
+
